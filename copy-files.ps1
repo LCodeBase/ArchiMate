61656082889c141631da6ed95096f8fdf44a9cb3
L CODE <leonardo.developer.figueiredo@gmail.com>
2025-03-29 13:06:29 -0300

    at

--- copy-files.ps1
+++ copy-files.ps1
@@ -1,6 +1,6 @@
 # Copiar arquivos do frontend/build para a raiz
 Copy-Item "frontend/build/static" -Destination "static" -Recurse -Force
-Copy-Item "frontend/build/logo.svg" -Destination "logo.svg" -Force
+Copy-Item "frontend/public/logo.svg" -Destination "logo.svg" -Force
 Copy-Item "frontend/build/manifest.json" -Destination "manifest.json" -Force
 Copy-Item "frontend/build/index.html" -Destination "index.html" -Force
 
