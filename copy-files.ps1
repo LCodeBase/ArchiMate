bf85c50f5602e035d8135570d9f531b292f242e2
L CODE <leonardo.developer.figueiredo@gmail.com>
2025-03-29 16:02:48 -0300

    at

--- copy-files.ps1
+++ copy-files.ps1
@@ -9,12 +9,10 @@ New-Item -ItemType Directory -Force -Path "ArchiMate"
 Copy-Item "frontend/build/static" -Destination "ArchiMate/static" -Recurse -Force
 Copy-Item "frontend/build/manifest.json" -Destination "ArchiMate/manifest.json" -Force
 Copy-Item "frontend/build/index.html" -Destination "ArchiMate/index.html" -Force
-Copy-Item ".htaccess" -Destination "ArchiMate/.htaccess" -Force
 
 # Copiar arquivos para a raiz (para compatibilidade)
 Copy-Item "ArchiMate/static" -Destination "static" -Recurse -Force
 Copy-Item "ArchiMate/manifest.json" -Destination "manifest.json" -Force
 Copy-Item "ArchiMate/index.html" -Destination "index.html" -Force
-Copy-Item "ArchiMate/.htaccess" -Destination ".htaccess" -Force
 
 Write-Host "Arquivos copiados com sucesso!"
